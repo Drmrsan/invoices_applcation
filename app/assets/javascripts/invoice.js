dd7197219caff6475fa08c71abafb08d33c23c46
Drmrsan <drmrsan@gmail.com>
2017-03-19 00:19:29 +0100

    JS

--- app/assets/javascripts/invoice.js
+++ app/assets/javascripts/invoice.js
@@ -1,40 +1,22 @@
 function calcAmount(){
   
-  $('.nested-fields').each(function(){
-    amount = $('.rate').val() * $('.qty').val();
+  // var row = $('.form-item').parent();
+  // var row = $('.nested-fields');
+  var rate = $('.rate').val();
+  var qty = $('.qty').val();
 
-    var result = isNaN(amount);
+  amount = rate * qty;
 
+  var result = isNaN(amount);
     if(result){
         $('.amount').val("");
     }
     else{
       $('.amount').val(amount);
     }
-  });
 }
   
-  
-
 function bindCalcAmount(){
   $('.rate').blur(calcAmount);
   $('.qty').blur(calcAmount);
 }
-
-
-  // $('.rate, .qty').keyup(function(){
-
-  //   var rate = parseInt($('.rate').val());
-  //   var qty = parseInt($('.qty').val());
-  //   var result = isNaN(qty * rate);
-
-  //       // if it's not a number put empty string, else calculate result
-  //   if(result){
-  //     $('.amount').val("");
-  //   }
-  //   else{
-  //     $('.amount').val(qty * rate);
-  //   }
-  // }).keyup();
-  
-
